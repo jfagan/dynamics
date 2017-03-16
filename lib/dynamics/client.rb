da580b828aa6676392ed0b35cf09f843ef565ee2
jfagan <jfagan365@gmail.com>
2017-03-16 09:24:43 -0700

    Refactored to use ETS inhouse Dynamics API

--- lib/dynamics/client.rb
+++ lib/dynamics/client.rb
@@ -3,36 +3,32 @@ module Dynamics
     attr_reader :api_token, :company_id, :site_id, :customer_code, :api_endpoint
 
     def initialize(customer_code)
-      @api_token = API_KEY
-      @company_id = COMPANY_ID
-      @site_id = SITE_ID
       @customer_code = customer_code
-      @api_endpoint = 'http://ws.etslabs.com:899/ctDynamicsSLREST/api/' #internal, non-ssl
+      @api_endpoint = ENV["ETS_DYNAMICS_API"]
 
-      if @customer_code.blank? || @api_token.blank? || @company_id.blank? || @site_id.blank?
-        raise ArgumentError, 'Customer Code, CompayID, SiteIT and API Auth Token are ALL required'
+      if @customer_code.blank?
+        raise ArgumentError, 'Customer Code is required'
       end
     end
 
     #TODO parse param start / end dates, allow for pagination params & to display paid invoices
     def get_invoices(params = {})
       if params[:start_date].present? && params[:end_date].present?
-        start_date = CGI.escape(params[:start_date].strftime("%m/%d/%Y"))
-        end_date = CGI.escape(params[:end_date].strftime("%m/%d/%Y"))
+        from_date = CGI.escape(params[:start_date].strftime("%m/%d/%Y"))
+        date_to = CGI.escape(params[:end_date].strftime("%m/%d/%Y"))
       else
-        start_date = CGI.escape(360.days.ago.strftime("%m/%d/%Y"))
-        end_date = CGI.escape(Date.today.strftime("%m/%d/%Y"))
+        from_date = CGI.escape(360.days.ago.strftime("%m/%d/%Y"))
+        date_to = CGI.escape(Date.today.strftime("%m/%d/%Y"))
       end
 
-      payload = {"name": "CuryDocBal", "Value": "&#33;=0"}
-      end_point = "#{@api_endpoint}financial/accountsReceivable/invoiceAndMemo/query?startDocDate=#{start_date}&endDocDate=#{end_date}&custID=#{@customer_code}"
+      end_point = "#{@api_endpoint}/api/clients/#{@customer_code}/invoices?date_from=#{from_date}&date_to=#{date_to}"
 
-      response = request(end_point, payload)
-      JSON.parse(response.body).map{ |dynamics_invoice| next if dynamics_invoice["DocType"] != "IN"; Dynamics::Invoice.new(dynamics_invoice) }.compact
+      response = request("GET", end_point, nil)
+      JSON.parse(response.body).map{ |dynamics_invoice| Dynamics::Invoice.new(dynamics_invoice) }.compact
     end
 
-    def get_invoice(params = {})
-      end_point = "#{@api_endpoint}financial/accountsReceivable/invoiceAndMemo/#{params[:batch_number]}/#{params[:invoice_number]}"
+    def get_invoice(invoice_number)
+      end_point = "#{@api_endpoint}/api/clients/#{@customer_code}/invoices/#{invoice_number}"
       payload = {}
 
       response = request("GET", end_point, payload)
@@ -41,10 +37,7 @@ module Dynamics
 
     def request_headers
       {'Content-Type': 'application/json',
-       'Accept': 'application/json',
-       'Authorization': "Basic #{@api_token}",
-       'CpnyID': "#{@company_id}",
-       'SiteID': "#{@site_id}" }
+       'Accept': 'application/json'}
     end
 
     def request(method = "POST", url, payload)
