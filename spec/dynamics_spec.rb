fba3d8a1f6d33e9bc0c1b059b1d9b180443a940b
jfagan <jfagan365@gmail.com>
2017-05-18 10:32:10 -0700

    Remove boilerplate specs

--- spec/dynamics_spec.rb
+++ spec/dynamics_spec.rb
@@ -1,11 +1,5 @@
 require "spec_helper"
 
 describe Dynamics do
-  it "has a version number" do
-    expect(Dynamics::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
