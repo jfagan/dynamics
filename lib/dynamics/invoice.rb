49cfd07519257420342bf17b672767e21a7f010d
jfagan <jfagan365@gmail.com>
2022-09-08 20:23:44 -0700

    Test Code to Simulate Closing a Batch in Dynamics i.e. UNPAID -> PENDING....after 5 mins...-> PAID

--- lib/dynamics/invoice.rb
+++ lib/dynamics/invoice.rb
@@ -2,6 +2,7 @@ module Dynamics
   class Invoice
     attr_accessor :order_number
     attr_accessor :paid_online
+    attr_accessor :date_paid_on 
 
     def initialize(dynamics_json_invoice)
       @invoice = dynamics_json_invoice
@@ -19,13 +20,13 @@ module Dynamics
       @invoice["amount_due"] = (new_status == "UNPAID" ? naked_balance : "0.00")
     end
 
-    def test_pending_status(date_paid = nil)
+    def test_pending_status
       #if invoice is paid, return 'PENDING' otherwise return 'UNPAID'
       status == "UNPAID" ? status : "PENDING"
 
       #react testng - simulate closing a batch in Dynamics 
       #***REMOVE FOR PRODUCTION / WHEN DOING PRODUCTION TEST***
-      if date_paid > 30.minutes.ago 
+      if !date_paid_on.nil? && date_paid_on < 5.minutes.ago 
         "PAID"
       end 
     end
